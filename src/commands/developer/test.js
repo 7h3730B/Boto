a06f0d20425112a9a0a0b7411cc5c1fe6580aef1
7h3730B <teoschulze5@gmail.com>
2020-04-16 16:10:27 +0200

    Added trigger/name, aliases, usage, desc, dms and cooldowns

---
+++ src/commands/developer/test.js
@@ -0,0 +1,13 @@
+module.exports.info = {
+    name: "test",
+    description: "cmds.developer.test.description",
+    usage: "cmds.developer.test.usage",
+    aliases: ["t", "te"],
+    dm: false,
+    args: false,
+    cooldown: 5,
+}
+
+module.exports.run = (client, message, args) => {
+    
+}
