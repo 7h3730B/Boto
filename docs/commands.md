ff7065e5d0ed67472ab3c9db52506573e4a9b63a
7h3730B <teoschulze5@gmail.com>
2020-04-17 11:27:31 +0200

    Testing if it works on Github

---
+++ docs/commands.md
@@ -0,0 +1,39 @@
+# Commands List
+
+## DEVELOPER
+- [test](#test)
+## INFO
+- [help](#help)
+
+ # Command Details 
+
+## DEVELOPER
+
+### test
+
+> Just for testing purposes
+
+| | |
+|---|---|
+| PermissionLevel | 1 |
+| Categorie | developer |
+| Aliases | t, te
+| NSFW | true |
+**Usage:**
+ <arg1> <arg2>
+---
+## INFO
+
+### help
+
+> Shows you all Categories and Commands
+
+| | |
+|---|---|
+| PermissionLevel | 1 |
+| Categorie | info |
+| available in DMs | true |
+**Usage:**
+ [Command] - Shows you information of a command
+[Categorie] - Shows you all Commands from a Categorie
+---
---
+++ wiki.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const {
+    join
+} = require("path");
+const {
+    getStringSync
+} = require("./src/utils/language");
+
+let markdown = "# Commands List\n\n";
+
+// get all Commands and Categories
+for (const categorie of fs.readdirSync(join(__dirname, "src/commands"))) {
+    let cmds = [];
+
+    markdown += "## " + categorie.toUpperCase() + "\n";
+
+    for (const cmd of fs.readdirSync(join(__dirname, "src/commands/" + categorie + "/"))) {
+        if (!cmd.endsWith(".js")) return;
+        markdown += "- [" + cmd.split('.')[0] + "](#" + cmd.split('.')[0] + ")\n";
+    }
+}
+
+markdown += "\n # Command Details \n\n";
+
+// Load Commands itsself
+for (const categorie of fs.readdirSync(join(__dirname, "src/commands"))) {
+    let cmds = [];
+
+    markdown += "## " + categorie.toUpperCase();
+
+    for (const cmd of fs.readdirSync(join(__dirname, "src/commands/" + categorie + "/"))) {
+        if (!cmd.endsWith(".js")) return;
+        const cmdreq = require(join(__dirname, "src/commands/" + categorie + "/" + cmd));
+        markdown += `\n\n### ${cmdreq.info.name}\n\n`; // title
+        markdown += `> ${getStringSync("", cmdreq.info.description)}\n\n`;
+        markdown += "| | |\n";
+        markdown += "|---|---|\n";
+        markdown += `| PermissionLevel | ${cmdreq.info.permission} |\n`;
+        markdown += `| Categorie | ${categorie} |\n`;
+        if (cmdreq.info.aliases) markdown += `| Aliases | ${cmdreq.info.aliases.join(", ")}\n`;
+        if (cmdreq.info.nsfw) markdown += `| NSFW | ${cmdreq.info.nsfw} |\n`;
+        if (cmdreq.info.dm) markdown += `| available in DMs | ${cmdreq.info.dm} |\n`;
+        if (cmdreq.info.usage) markdown += `**Usage:**\n ${getStringSync("", cmdreq.info.usage)}\n`;
+    }
+    markdown += "---\n";
+}
+
+fs.writeFile(join(__dirname, "/docs/commands.md"), markdown, (err) => {
+    if (err) return console.log("FAILED: to create wiki: " + err);
+    console.log("successfully created wiki");
+});
