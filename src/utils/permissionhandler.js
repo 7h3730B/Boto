01fb1381bf9c108538778141d88e4fdb81ceae42
7h3730B <teoschulze5@gmail.com>
2020-05-08 19:29:27 +0200

    Commented out part of the Permission Handler, because of code quality

--- src/utils/permissionhandler.js
+++ src/utils/permissionhandler.js
@@ -8,16 +8,16 @@ async function changePermissionlvl(user, newlvl) {
 async function getPermissionlvl(user, guild) {
     if (owners.includes(user.id)) return 10;
     if (guild) if (user = guild.ownerID) return 9;
-    if (false) return 8; // Could be used for things
-    if (false) return 7; // Could be used for things
-    if (false) return 6; // Could be used for things
-    if (false) return 5; // Could be used for things
-    if (false) return 4; // Could be used for things
-    if (false) return 3; // Could be used for things like Admins
-    if (false) return 2; // Could be used for things like Mods
+    // if (false) return 8; // Could be used for things
+    // if (false) return 7; // Could be used for things
+    // if (false) return 6; // Could be used for things
+    // if (false) return 5; // Could be used for things
+    // if (false) return 4; // Could be used for things
+    // if (false) return 3; // Could be used for things like Admins
+    // if (false) return 2; // Could be used for things like Mods
     // TODO: Database request checks if banned from the bot completly or just on a guild
     if (true) return 1; // TODO: Can use the bot normaly
-    if (false) return 0; // TODO: Can't use the bot
+    // if (false) return 0; // TODO: Can't use the bot
 }
 
 module.exports = {
